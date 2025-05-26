ed22db8e10216c3dc3d8ae579aa9968e9505d239
Ilia Dubovik <askdubovik@gmail.com>
2025-05-26 21:51:32 +0200

    test: install vitest; add path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
+import { resolve } from "path";
+
 export default {
   root: "./src",
+  resolve: {
+    alias: {
+      "@src": resolve(__dirname, "src"),
+      "@tests": resolve(__dirname, "src/tests"),
+    },
+  },
   build: {
     outDir: "../dist",
   },
   publicDir: "public",
   server: {
     port: 6969,
+  },
+  test: {
+    dir: "./tests/unit/",
+    environment: "jsdom",
   },
 };
